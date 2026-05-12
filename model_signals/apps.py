316f88158a851c87de33ce6a5e5583f56444bd89
djumanov <djumanovdev@gmail.com>
2026-05-12 19:25:45 +0500

    signals

--- model_signals/apps.py
+++ model_signals/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class ModelSignalsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'model_signals'
+
+    def ready(self):
+        import model_signals.signals
+        
---
+++ model_signals/signals.py
@@ -0,0 +1,19 @@
+from django.db.models.signals import post_save, pre_delete
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(pre_delete, sender=User)
+def delete_user_profile(sender, instance, **kwargs):
+    try:
+        instance.profile.delete()
+    except Profile.DoesNotExist:
+        pass
+    
