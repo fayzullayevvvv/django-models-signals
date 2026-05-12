85fe52aaef8f88eebfd25696bc9defa6b77a6014
djumanov <djumanovdev@gmail.com>
2026-05-12 19:56:28 +0500

    signals

--- model_signals/signals.py
+++ model_signals/signals.py
@@ -1,4 +1,6 @@
-from django.db.models.signals import post_save, pre_delete
+from datetime import datetime
+
+from django.db.models.signals import post_save, pre_delete, post_delete
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile
@@ -17,3 +19,15 @@ def delete_user_profile(sender, instance, **kwargs):
     except Profile.DoesNotExist:
         pass
     
+
+@receiver(post_save, sender=Profile)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        print(f"Profile succesfully created at {datetime.now()}")
+    else:
+        print(f"Profile succesfully updated at {datetime.now()}")
+
+
+@receiver(post_delete, sender=Profile)
+def delete_user_profile(sender, instance, **kwargs):
+    print(f"Profile deleted at {datetime.now()}")
